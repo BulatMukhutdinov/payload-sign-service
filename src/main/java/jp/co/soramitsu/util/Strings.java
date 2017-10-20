5a4af1545b0cbe77e89048958fa68bc3079f9fdf
bulatmukhutdinov <bulatmm@yandex.ru>
2017-10-20 12:40:10 +0300

    Initial commit

---
+++ src/main/java/jp/co/soramitsu/util/Strings.java
@@ -0,0 +1,15 @@
+package jp.co.soramitsu.util;
+
+public class Strings {
+
+    private Strings() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+
+    // Regexps
+    public static final String REGEX_ACCOUNT_NAME = "[a-z]{1,7}";
+    public static final String REGEX_DOMAIN_ID = "[a-z]{1,9}";
+
+    // Errors
+    public static final String ERROR_INVALID_ARGUMENT = "Invalid field '%s'. It has to match pattern %s";
+}
