6476b318a86bab4789f54bd22fe11db8b21d3fd7
bulatmukhutdinov <bulatmm@yandex.ru>
2017-12-14 16:23:19 +0300

    Ability to add additional account info

--- src/main/java/jp/co/soramitsu/App.java
+++ src/main/java/jp/co/soramitsu/App.java
@@ -1,8 +1,8 @@
 package jp.co.soramitsu;
 
 import com.google.protobuf.ByteString;
+import com.google.protobuf.Empty;
 import io.grpc.*;
-import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import iroha.protocol.*;
 import jp.co.soramitsu.payloadsignservice.*;
@@ -20,19 +20,24 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.Properties;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
 
-import static jp.co.soramitsu.payloadsignservice.Status.*;
+import static jp.co.soramitsu.payloadsignservice.StatusResponse.Status.*;
 import static jp.co.soramitsu.util.Const.*;
 import static jp.co.soramitsu.util.LambdaExceptionUtil.rethrowConsumer;
 
 public class App {
+
     private static final Logger logger = Logger.getLogger(App.class.getName());
 
-    private final ManagedChannel channel;
-    private final CommandServiceGrpc.CommandServiceBlockingStub blockingStub;
+    private static AtomicLong counter = new AtomicLong();
+
+    private final CommandServiceGrpc.CommandServiceBlockingStub commandServiceBlockingStub;
+
+    private final QueryServiceGrpc.QueryServiceBlockingStub queryServiceBlockingStub;
 
     private Optional<Server> server;
 
@@ -40,13 +45,14 @@ public class App {
      * Create a RouteGuide server listening on {@code port}
      */
     private App() {
-        channel = ManagedChannelBuilder.forAddress(
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(
                 System.getProperty("iroha.address"),
                 Integer.parseInt(System.getProperty("iroha.port")))
                 // Channels are secure by default (via SSL/TLS). Disable TLS to avoid needing certificates.
                 .usePlaintext(true)
                 .build();
-        blockingStub = CommandServiceGrpc.newBlockingStub(channel);
+        commandServiceBlockingStub = CommandServiceGrpc.newBlockingStub(channel);
+        queryServiceBlockingStub = QueryServiceGrpc.newBlockingStub(channel);
     }
 
     /**
@@ -95,17 +101,94 @@ public class App {
 
     private class PayloadSignService extends PayloadSignServiceGrpc.PayloadSignServiceImplBase {
 
+        @Override
+        public void setAccountDetail(SetAccountDetailRequest request, StreamObserver<Empty> responseObserver) {
+            logger.log(Level.INFO, () -> "setAccountDetail request is received");
+            logger.log(Level.INFO, () -> "accountId = '" + request.getAccountId() + "'" +
+                    "key = '" + request.getKey() + "'" +
+                    "value = '" + request.getValue() + "'");
+            try {
+                BlockOuterClass.Transaction.Payload payload = buildTransactionPayload(buildSetAccountDetailCommand(
+                        request.getAccountId(), request.getKey(), request.getValue()));
+                SignatureResult result = buildSignature(payload.toByteArray());
+                BlockOuterClass.Transaction transaction = BlockOuterClass.Transaction
+                        .newBuilder()
+                        .setPayload(payload)
+                        .addSignature(result.signature)
+                        .build();
+                commandServiceBlockingStub.torii(transaction);
+                responseObserver.onNext(Empty.newBuilder().build());
+                responseObserver.onCompleted();
+                logger.log(Level.INFO, () -> "Set account detail request has been successfully processed");
+            } catch (StatusRuntimeException | NoSuchAlgorithmException | SignatureException | InvalidKeyException exception) {
+                logger.log(Level.SEVERE, () -> "Exception raised during account setting request");
+                onError(exception, responseObserver);
+            }
+        }
+
+        private List<Commands.Command> buildSetAccountDetailCommand(String accountId, String key, String value) {
+            Commands.SetAccountDetail setAccountDetail = Commands.SetAccountDetail
+                    .newBuilder()
+                    .setAccountId(accountId)
+                    .setKey(key)
+                    .setValue(value)
+                    .build();
+            return Collections.singletonList(Commands.Command.newBuilder().setSetAccountDetail(setAccountDetail).build());
+        }
+
+        @Override
+        public void getAccount(GetAccountRequest request, StreamObserver<GetAccountResponse> responseObserver) {
+            logger.log(Level.INFO, () -> "getAccount request is received");
+            logger.log(Level.INFO, () -> "accountId = '" + request.getAccountId() + "'");
+            try {
+                Responses.QueryResponse queryResponse = queryServiceBlockingStub.find(buildQuery(request.getAccountId()));
+                Responses.Account irohaAccount = queryResponse.getAccountResponse().getAccount();
+                GetAccountResponse.Account account = GetAccountResponse.Account.newBuilder()
+                        .setAccountId(irohaAccount.getAccountId())
+                        .setDomainId(irohaAccount.getDomainId())
+                        .setQuorum(irohaAccount.getQuorum())
+                        .setJsonData(irohaAccount.getJsonData())
+                        .build();
+                responseObserver.onNext(GetAccountResponse.newBuilder().setAccount(account).build());
+                responseObserver.onCompleted();
+                logger.log(Level.INFO, () -> "Get account request has been successfully processed");
+            } catch (NoSuchAlgorithmException | SignatureException | InvalidKeyException | StatusRuntimeException exception) {
+                logger.log(Level.SEVERE, () -> "Exception raised during get account request");
+                onError(exception, responseObserver);
+            }
+        }
+
+        private Queries.Query buildQuery(String accountId) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+            Queries.Query.Payload payload = buildQueryPayload(accountId);
+            SignatureResult signature = buildSignature(payload.toByteArray());
+            return Queries.Query
+                    .newBuilder()
+                    .setPayload(payload)
+                    .setSignature(signature.signature)
+                    .build();
+        }
+
+        private Queries.Query.Payload buildQueryPayload(String accountId) {
+            return Queries.Query.Payload
+                    .newBuilder()
+                    .setCreatedTime(System.currentTimeMillis())
+                    .setCreatorAccountId(System.getProperty("this.id"))
+                    .setGetAccount(Queries.GetAccount.newBuilder().setAccountId(accountId).build())
+                    .setQueryCounter(counter.getAndIncrement())
+                    .build();
+        }
 
         @Override
         public void status(StatusRequest statusRequest, StreamObserver<StatusResponse> responseObserver) {
-            logger.log(Level.INFO, () -> "status is requested by hash");
-            try {
-                Endpoint.TxStatusRequest request = Endpoint.TxStatusRequest.newBuilder()
-                        .setTxHash(statusRequest.getHash())
-                        .build();
-                Endpoint.TxStatus txStatus = blockingStub.status(request).getTxStatus();
-                jp.co.soramitsu.payloadsignservice.Status status;
+            logger.log(Level.INFO, () -> "status request is received");
+            logger.log(Level.INFO, () -> "hash = '" + statusRequest.getHash() + "'");
 
+            Endpoint.TxStatusRequest request = Endpoint.TxStatusRequest.newBuilder()
+                    .setTxHash(statusRequest.getHash())
+                    .build();
+            Endpoint.TxStatus txStatus = commandServiceBlockingStub.status(request).getTxStatus();
+            jp.co.soramitsu.payloadsignservice.StatusResponse.Status status;
+            try {
                 switch (txStatus) {
                     case STATELESS_VALIDATION_FAILED:
                         status = STATELESS_VALIDATION_FAILED;
@@ -132,20 +215,19 @@ public class App {
                         status = UNRECOGNIZED;
                 }
 
-                logger.log(Level.INFO, () -> "Status request has been successfully processed");
-                logger.log(Level.INFO, () -> "Status is " + status.name());
                 responseObserver.onNext(StatusResponse.newBuilder().setStatus(status).build());
                 responseObserver.onCompleted();
-            } catch (StatusRuntimeException e) {
+                logger.log(Level.INFO, () -> "Status request has been successfully processed");
+                logger.log(Level.INFO, () -> "Status is " + status.name());
+            } catch (StatusRuntimeException exception) {
                 logger.log(Level.SEVERE, () -> "Exception raised during status request");
-                logger.log(Level.SEVERE, e::getLocalizedMessage);
-                responseObserver.onError(e);
+                onError(exception, responseObserver);
             }
         }
 
         @Override
         public void createAccount(CreateAccountRequest request, StreamObserver<CreateAccountResponse> responseObserver) {
-            logger.log(Level.INFO, () -> "new createAccount request is received");
+            logger.log(Level.INFO, () -> "createAccount request is received");
             logger.log(Level.INFO, () -> "accountName = '" + request.getAccountName()
                     + "' domainId = '" + request.getDomainId() + "'");
             // Get and validate account name
@@ -168,36 +250,35 @@ public class App {
 
             try {
                 byte[] hash = createAccount(accountName, domainId, request.getMainPubkey());
-                logger.log(Level.INFO, () -> "Create account request has been successfully processed");
                 responseObserver.onNext(CreateAccountResponse.newBuilder().setHash(ByteString.copyFrom(hash)).build());
                 responseObserver.onCompleted();
+                logger.log(Level.INFO, () -> "Create account request has been successfully processed");
             } catch (StatusRuntimeException | NoSuchAlgorithmException | SignatureException | InvalidKeyException exception) {
                 logger.log(Level.SEVERE, () -> "Exception raised during account creation");
-                logger.log(Level.SEVERE, exception::getLocalizedMessage);
-                responseObserver.onError(exception);
+                onError(exception, responseObserver);
             }
         }
 
         private byte[] createAccount(String accountName, String domainId, ByteString publicKey)
                 throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
-            BlockOuterClass.Transaction.Payload payload = buildPayload(accountName, domainId, publicKey);
+            BlockOuterClass.Transaction.Payload payload = buildTransactionPayload(buildCreateAccountCommand(accountName, domainId, publicKey));
             SignatureResult result = buildSignature(payload.toByteArray());
 
             BlockOuterClass.Transaction transaction = BlockOuterClass.Transaction
                     .newBuilder()
                     .setPayload(payload)
-                    .addAllSignature(result.signature)
+                    .addSignature(result.signature)
                     .build();
-            blockingStub.torii(transaction);
+            commandServiceBlockingStub.torii(transaction);
             return result.hash;
         }
 
-        private BlockOuterClass.Transaction.Payload buildPayload(String accountName, String domainId, ByteString publicKey) {
+        private BlockOuterClass.Transaction.Payload buildTransactionPayload(List<Commands.Command> commands) {
             return BlockOuterClass.Transaction.Payload
                     .newBuilder()
-                    .addAllCommands(buildCommand(accountName, domainId, publicKey))
+                    .addAllCommands(commands)
                     .setCreatorAccountId(System.getProperty("this.id"))
-                    .setTxCounter(0)
+                    .setTxCounter(counter.getAndIncrement())
                     .setCreatedTime(System.currentTimeMillis())
                     .build();
         }
@@ -229,10 +310,10 @@ public class App {
                     .setPubkey(ByteString.copyFrom(hexStringToByteArray(System.getProperty("this.public_key"))))
                     .build();
 
-            return new SignatureResult(msgToSign, Collections.singletonList(signature));
+            return new SignatureResult(msgToSign, signature);
         }
 
-        private List<Commands.Command> buildCommand(String accountName, String domainId, ByteString publicKey) {
+        private List<Commands.Command> buildCreateAccountCommand(String accountName, String domainId, ByteString publicKey) {
             Commands.CreateAccount createAccount = Commands.CreateAccount
                     .newBuilder()
                     .setAccountName(accountName)
@@ -256,12 +337,17 @@ public class App {
 
             private byte[] hash;
 
-            private List<Primitive.Signature> signature;
+            private Primitive.Signature signature;
 
-            SignatureResult(byte[] hash, List<Primitive.Signature> signature) {
+            SignatureResult(byte[] hash, Primitive.Signature signature) {
                 this.hash = hash;
                 this.signature = signature;
             }
+        }
+
+        private void onError(Exception exception, StreamObserver responseObserver) {
+            logger.log(Level.SEVERE, exception::getLocalizedMessage);
+            responseObserver.onError(exception);
         }
     }
 }
